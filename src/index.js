d8d8cbc9bb6a24827ce4f8ae2b26599828b33aa2
evilkeil <114153280+evilkeil@users.noreply.github.com>
2023-10-30 15:37:40 +0900

    finsih up the basic funtionality of the slider

--- src/index.js
+++ src/index.js
@@ -1,48 +1,48 @@
 import './style.css'
 
-// import Icon from './kv_01-min.jpg';
 
-// const container = document.querySelector('.carousel-container');
-// const slide = document.querySelector('.slides');
+const container = document.querySelector('.carousel-container');
+const slide = document.querySelector('.slides');
 
-// let slides = document.querySelectorAll('.slide');
-// let index = 1;
-// const interval = 3000;
+let slides = document.querySelectorAll('.slide');
+let index = 1;
+const interval = 3000;
 
-// const firstClone = slides[0].cloneNode(true);
-// const lastClone = slides[slides.length - 1].cloneNode(true);
+const firstClone = slides[0].cloneNode(true);
+const lastClone = slides[slides.length - 1].cloneNode(true);
 
-// firstClone.id = 'first-clone';
-// lastClone.id = 'last-clone';
-
-// slide.append(firstClone);
-// slide.prepend(lastClone);
+firstClone.id = 'first-clone';
+lastClone.id = 'last-clone';
 
 
-// const slidesWidth = slides[index].clientWidth;
-
-// const getSlides = ()=> document.querySelectorAll('.slide');
-
-// slide.style.transform= `translateX(${-slidesWidth * index}px)`;
-
-// const startSlide = () => {
-//     setInterval(()=>{
-//         index++;
-//         slide.style.transform= `translateX(${-slidesWidth * index}px)`;
-//         slide.style.transition= `0.7s`
-//     },interval)
-// }
-
-// slide.addEventListener('transitionend',()=>{
-//     slides = getSlides();
-//     if(slides[index].id === firstClone.id){
-//         slide.style.transition= `none`;
-//         index = 1;
-//         slide.style.transform= `translateX(${-slidesWidth * index}px)`;
-//     }
-// })
+slide.append(firstClone);
+slide.prepend(lastClone);
 
 
+const slidesWidth = slides[index].clientWidth;
+
+const getSlides = ()=> document.querySelectorAll('.slide');
+
+slide.style.transform= `translateX(${-slidesWidth * index}px)`;
+
+const startSlide = () => {
+    setInterval(()=>{
+        index++;
+        slide.style.transform= `translateX(${-slidesWidth * index}px)`;
+        slide.style.transition= `0.7s`
+    },interval)
+}
+
+slide.addEventListener('transitionend',()=>{
+    slides = getSlides();
+    if(slides[index].id === firstClone.id){
+        slide.style.transition= `none`;
+        index = 1;
+        slide.style.transform= `translateX(${-slidesWidth * index}px)`;
+    }
+})
 
 
-// startSlide();
+
+
+startSlide();
