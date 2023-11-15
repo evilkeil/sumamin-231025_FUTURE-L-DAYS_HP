adc1176712d8f87e7e974e7fc365207258710a1e
evilkeil <114153280+evilkeil@users.noreply.github.com>
2023-11-15 15:51:30 +0900

    add animations to company

--- src/index.js
+++ src/index.js
@@ -175,34 +175,8 @@ tl2
   .set(".service-main", {backgroundColor:"transparent"})
   .set("#service", {backgroundColor:"#F0F5F5"})
 
-   //animations for the company section
-
-
-
-  //  let tl4 = gsap.timeline({
-  //   scrollTrigger: {
-  //     trigger: ".company-hero", //edit
-  //     start: "top top",
-  //     end:"+=200",
-  //     scrub: 3,
-  //     pin: true,
-  //    markers: false
-  //   }
-  // });
   
-  // tl4.to(".company-dot", {scale:0.5})
-  //   .to(".company-title", {x:"50vw", xPercent:-50}, 0)
-  //   .to(".company-dot", {scale:1})
-  //   .to(".company-title", {x:"150vw", xPercent:-50}, 0)
-  //   .to(".company-main", {y:"-300px"})
-  //   .set(".company-main", {backgroundColor:"#F0F5F5"},0)
 
-  
-    // tl4.to(".company-dot", {scale:1})
-    //   .to(".company-title", {x:"150vw", xPercent:100}, 0)
-    //   .to(".company-main", {y:"-300px"})
-    //   .set(".company-main", {backgroundColor:"#F0F5F5"},0)
-    
     //topics animations
 
     //old
@@ -248,7 +222,7 @@ const master = gsap.timeline()
 master.add(topicTop())
   
    
-//company section animations mk2
+//company section animations 
 
 let tl4 = gsap.timeline({
   scrollTrigger: {
